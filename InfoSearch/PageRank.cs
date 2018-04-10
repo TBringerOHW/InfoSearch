623d052c790a7dc6c0d119919066cd14af512ce5
DESKTOP-8F9H0F1\TBringerOHW <1closed1@gmail.com>
2018-04-10 12:50:41 +0300

    Raw/Untested PageRank

---
+++ InfoSearch/PageRank.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoSearch {
+    class PageRank {
+        private static readonly double DIFFERENCE = 1.0e-4, ATTENUATION_COEF = 0.85;
+        private int[][] matrix;
+        private int[] sumLinks;
+        private int length;
+        //decimal df = new decimal("#.###");
+
+        public double[] getPageRank(int[][] matrix) {
+            length = matrix.Length;
+            this.matrix = matrix;
+            double[] pageRankOld = new double[this.length];
+            double[] pageRank = new double[this.length];
+            bool stop;
+            //this.df.setRoundingMode(RoundingMode.HALF_UP);
+            double sum;
+
+            getSumLinks();
+
+            for(int i = 0; i < this.length; i++) {
+                pageRankOld[i] = 1;
+            }
+
+            do {
+                for(int j = 0; j < this.length; j++) {
+                    sum = 0;
+                    pageRank[j] = 1 - ATTENUATION_COEF;
+                    for(int i = 0; i < this.length; i++) {
+                        if(this.matrix[i][j] > 0 && this.sumLinks[i] != 0) {
+                            sum += pageRankOld[i] / this.sumLinks[i];
+                        }
+                        else {
+                            sum += 0;
+                        }
+                    }
+                    pageRank[j] += ATTENUATION_COEF * sum;
+                    //pageRank[j] += Double.valueOf(df.format(ATTENUATION_COEF * sum).replace(',', '.'));
+                }
+
+                stop = checkStop(pageRankOld, pageRank);
+                pageRankOld = pageRank;
+            } while(!stop);
+
+            return pageRank;
+        }
+
+        private bool checkStop(double[] pageRankOld, double[] pageRank) {
+            bool result = true;
+            double range;
+            for(int i = 0; i < length; i++) {
+                range = pageRankOld[i] - pageRank[i];
+                if(range < 0) {
+                    range = -range;
+                }
+                if((range) > DIFFERENCE) {
+                    result = false;
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        private void getSumLinks() {
+            sumLinks = new int[length];
+
+            for(int i = 0; i < length; i++) {
+                for(int j = 0; j < length; j++) {
+                    sumLinks[i] += matrix[i][j];
+                }
+            }
+        }
+    }
+}
