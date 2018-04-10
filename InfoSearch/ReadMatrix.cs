ba4f74a9264a375d5bb3d2d4d97247f4f9c54630
KamilMukhametzyanov <qweekman@gmail.com>
2018-04-10 23:37:00 +0300

    - Добавлены методы чтения и записи

---
+++ InfoSearch/ReadMatrix.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoSearch
+{
+    class ReadMatrix
+    {
+        private int[,] matrix;
+        private List<String> rootLinks = new List<String>();
+        private List<String> matrixLines = new List<String>();
+
+        public Boolean readMatrix(String fileName)
+        {
+            bool isFile;
+            try
+            {
+                isFile = true;
+                int index;
+                bool endMatrix = false;
+
+                StreamReader reader = new StreamReader(fileName);
+
+                String line = reader.ReadLine();
+
+                while (line != null)
+                {
+                    if (line.Equals(""))
+                    {
+                        endMatrix = true;
+                        line = reader.ReadLine();
+                    }
+                    if (!endMatrix)
+                    {
+                        matrixLines.Add(line);
+                    }
+                    else
+                    {
+                        index = line.IndexOf(')');
+                        rootLinks.Add(line.Substring(index + 1));
+                    }
+
+                    line = reader.ReadLine();
+                }
+                this.createMatrix();
+
+            }
+            catch (FileNotFoundException)
+            {
+                isFile = false;
+                return isFile;
+            }
+            catch (IOException)
+            {
+                isFile = false;
+                return isFile;
+            }
+            return isFile;
+        }
+
+        public int[,] getMatrix()
+        {
+            return matrix;
+        }
+
+        public List<String> getRootLinks()
+        {
+            return rootLinks;
+        }
+
+        private void createMatrix()
+        {
+            int size = matrixLines.Count();
+            this.matrix = new int[size, size];
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    matrix[i, j] = Int32.Parse(matrixLines.ElementAt(i).Substring(j, 1));
+                }
+            }
+        }
+    }
+}
---
+++ InfoSearch/WriteMatrixCoord.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoSearch
+{
+    public class WriteMatrixCoord
+    {
+        public void writeMatrixCoord(List<int> values, List<int> iIndex, List<int> jIndex, List<String> rootLinks, String fileName)
+        {
+            String info = "";
+
+            for (int i = 0; i < values.Count(); i++)
+            {
+                info = String.Concat(info, values.ElementAt(i) + "-");
+            }
+
+            info = String.Concat(info, "\r\n");
+
+            for (int i = 0; i < iIndex.Count(); i++)
+            {
+                info = String.Concat(info, iIndex.ElementAt(i) + "-");
+            }
+
+            info = String.Concat(info, "\r\n");
+
+            for (int i = 0; i < jIndex.Count(); i++)
+            {
+                info = String.Concat(info, jIndex.ElementAt(i) + "-");
+            }
+
+            info = String.Concat(info, "\r\n");
+
+            for (int i = 0; i < rootLinks.Count(); i++)
+            {
+                info = String.Concat(info, "\r\n");
+                info = String.Concat(info, i + ") " + rootLinks.ElementAt(i));
+            }
+
+            try
+            {
+                StreamWriter writer = new StreamWriter(fileName);
+                writer.Write(info);
+                writer.Flush();
+                writer.Close();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
